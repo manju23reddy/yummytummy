002f78822c22a22e266cdf325a1681051f6cf71b
dineshpogula <dineshrpogula@gmail.com>
2021-10-30 12:15:13 -0400

    DP:: Splash screen chages WIP

--- app/src/main/java/com/manju23reddy/yummytummy/MainActivity.kt
+++ app/src/main/java/com/manju23reddy/yummytummy/MainActivity.kt
@@ -2,10 +2,16 @@ package com.manju23reddy.yummytummy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Handle the splash screen transition.
+        setTheme(R.style.Theme_YummyTummy)
         setContentView(R.layout.activity_main)
+
     }
 }
